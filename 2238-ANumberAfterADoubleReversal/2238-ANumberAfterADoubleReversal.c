87d633e387ba4b589e1519b424fd081262bc63a0
Bhavesh Santoshkumar Tupe <bhaveshtupe06@gmail.com>
2025-10-04 17:45:03 +0530

    Create 2238-ANumberAfterADoubleReversal

---
+++ 2238-ANumberAfterADoubleReversal/2238-ANumberAfterADoubleReversal.c
@@ -0,0 +1,13 @@
+// Last updated: 10/4/2025, 5:45:04 PM
+bool isSameAfterReversals(int num) {
+    if(num==0){
+        return true;
+    }
+    if(num%10==0){
+        return false;
+    }
+    else{
+     return true;
+    }
+     
+}
