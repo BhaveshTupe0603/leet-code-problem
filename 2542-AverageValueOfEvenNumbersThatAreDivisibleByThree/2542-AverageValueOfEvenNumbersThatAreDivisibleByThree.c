cf2ac15d169720cbf361ccf4fda0cb1799c60e6a
Bhavesh Santoshkumar Tupe <bhaveshtupe06@gmail.com>
2025-10-04 17:44:46 +0530

    Create 2542-AverageValueOfEvenNumbersThatAreDivisibleByThree

---
+++ 2542-AverageValueOfEvenNumbersThatAreDivisibleByThree/2542-AverageValueOfEvenNumbersThatAreDivisibleByThree.c
@@ -0,0 +1,17 @@
+// Last updated: 10/4/2025, 5:44:47 PM
+int averageValue(int* nums, int numsSize) {
+    int sum = 0;
+    int count = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] % 2 == 0 && nums[i] % 3 == 0) {
+            sum += nums[i];
+            count++;
+        }
+    }
+
+    if (count == 0)
+        return 0;
+
+    return sum / count;
+}
