7217733c3ff6f04b62dcc28a79fd28364cd3e784
Bhavesh Santoshkumar Tupe <bhaveshtupe06@gmail.com>
2025-10-04 17:44:57 +0530

    Create 2491-SmallestEvenMultiple

---
+++ 2491-SmallestEvenMultiple/2491-SmallestEvenMultiple.py
@@ -0,0 +1,8 @@
+# Last updated: 10/4/2025, 5:44:58 PM
+class Solution(object):
+    def smallestEvenMultiple(self, n):
+        if(n%2==0):
+            return n
+        else:
+            return n*2
+        
