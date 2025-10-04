834dab09b66bbe93981657a2453e88ec7e3cea27
Bhavesh Santoshkumar Tupe <bhaveshtupe06@gmail.com>
2025-10-04 17:46:07 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.c
@@ -0,0 +1,8 @@
+// Last updated: 10/4/2025, 5:46:08 PM
+int singleNumber(int* nums, int numsSize) {
+    int result = 0;
+    for (int i = 0; i < numsSize; i++) {
+        result ^= nums[i];
+    }
+    return result;
+}
