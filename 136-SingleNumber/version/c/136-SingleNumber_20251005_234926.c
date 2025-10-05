875c05b0ff3ab0712cc5ce6e643c5b4b99cf0c9a
Bhavesh Santoshkumar Tupe <bhaveshtupe06@gmail.com>
2025-10-05 23:49:27 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/version/c/136-SingleNumber_20251005_234926.c
@@ -0,0 +1,15 @@
+// Last updated: 10/5/2025, 11:49:26 PM
+int majorityElement(int* nums, int numsSize) {
+    int count = 0, candidate = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (count == 0) {
+            candidate = nums[i];
+            count = 1;
+        } else if (nums[i] == candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+    return candidate;
+}
