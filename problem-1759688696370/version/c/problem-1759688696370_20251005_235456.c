41996f81a4bf34f7d4aedd99a574f137b6e34ba4
Bhavesh Santoshkumar Tupe <bhaveshtupe06@gmail.com>
2025-10-05 23:54:57 +0530

    Create

---
+++ problem-1759688696370/version/c/problem-1759688696370_20251005_235456.c
@@ -0,0 +1,12 @@
+// Last updated: 10/5/2025, 11:54:56 PM
+bool checkPerfectNumber(int num) {
+    if (num <= 1) return false;
+    int sum = 1;
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) {
+            sum += i;
+            if (i != num / i) sum += num / i;
+        }
+    }
+    return sum == num;
+}
